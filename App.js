3589e5669c538c801dc9ca3d7ac39bfca53e59e0
Fahad Hossain <set.name.fahad@gmail.com>
2020-07-11 14:55:40 +0600

    re-init with expo

--- App.js
+++ App.js
@@ -1,5 +1,21 @@
-/**
- * Do not modify this file - it is a proxy to your `App.re` file
- * located in the `src/` folder.
- */
-export {app as default} from './src/App.bs.js';
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
