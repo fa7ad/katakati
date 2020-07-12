bb7ebb6a61ba23f36993f7794a855554859d16e5
Fahad Hossain <set.name.fahad@gmail.com>
2020-07-12 23:37:51 +0600

    remove tests

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-import { registerRootComponent } from 'expo';
+import { registerRootComponent } from "expo";
 
-import {make as App} from './src/App.bs';
+import { make as App } from "./src/App.bs";
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in the Expo client or in a native build,
