8f15adc1c894632d491d2cd7744ddc8589672fe4
Fahad Hossain <set.name.fahad@gmail.com>
2020-07-12 23:33:27 +0600

    remove unused JS files

--- __tests__/App.js
+++ __tests__/App.js
@@ -1,6 +1,6 @@
 import 'react-native';
 import React from 'react';
-import App from '../App';
+import { make as App } from '../src/App.bs';
 
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
